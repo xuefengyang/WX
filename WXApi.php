7869669e61288ec63d433b99899d5a68dd0174ca
杨雪峰 <yangxuefeng@yangxuefengdeMacBook-Pro.local>
2018-03-16 20:50:42 +0800

    test

--- WXApi.php
+++ WXApi.php
@@ -52,16 +52,15 @@
 		}
 
 		public function getRemoteData($url){
-			$curl = curl_init();
+		    $curl = curl_init();
 		    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		    curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-	        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-	        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-	        curl_setopt($curl, CURLOPT_URL, $url);
-	        $res = curl_exec($curl);
-	        curl_close($curl);
-	        return $res;
-
+	            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+	            curl_setopt($curl, CURLOPT_URL, $url);
+	            $res = curl_exec($curl);
+	            curl_close($curl);
+	            return $res;
 	        //return file_get_contents($url);
 		}
 
